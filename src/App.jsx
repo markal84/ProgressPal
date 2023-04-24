3d390ad9464a15dd71e07a10ac7acfd663605b80
Mar Kal <marc@example.com>
2023-04-24 17:29:48 +0200

    feat: created basic structure for adding workout

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,62 @@
+import { useState } from 'react'
 import Workout from './components/Workout'
 
 function App(props) {
-  const { workouts } = props
+  const [workouts, setWorkouts] = useState(props.workouts)
+  const [day, setDay] = useState('')
+  const [date, setDate] = useState('')
+  // const [showAll, setShowAll] = useState(false)
+
+  function handleAddWorkout(event) {
+    event.preventDefault()
+
+    const newWorkout = {
+      id: workouts.length + 1,
+      day,
+      date,
+      exercises: []
+    }
+
+    console.log(newWorkout)
+
+    setWorkouts([...workouts, newWorkout])
+
+    setDay('')
+    setDate('')
+  }
+
+  function handleDayChange(event) {
+    setDay(event.target.value)
+    console.log(event.target.value)
+  }
+
+  function handleDateChange(event) {
+    setDate(event.target.value)
+  }
+
+  // const workoutsToShow = showAll ? workouts : workouts.filter((workout) => workout.important)
 
   return (
     <>
       <div>
         <h1>Gym progress app</h1>
+        <div>
+          <button>This button will show hide some data</button>
+        </div>
+        <form onSubmit={handleAddWorkout}>
+          <label htmlFor="day">Day:</label>
+          <input type="text" id="day" value={day} onChange={handleDayChange} />
+
+          <label htmlFor="date">Date:</label>
+          <input
+            type="text"
+            id="date"
+            value={date}
+            onChange={handleDateChange}
+          />
+
+          <button type="submit">Add Workout</button>
+        </form>
         <div>
           {workouts.map((workout) => (
             <Workout key={workout.id} workout={workout} />
