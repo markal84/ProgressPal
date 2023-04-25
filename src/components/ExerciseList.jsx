710a5e7fa1a5d493ab937a584446199b2b6078d1
Mar Kal <marc@example.com>
2023-04-25 11:01:36 +0200

    refactor: move exercises list to component

---
+++ src/components/ExerciseList.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+import Exercise from './Exercise'
+import AddExerciseForm from './forms/AddExerciseForm'
+
+export default function ExerciseList({ workout }) {
+  const [exercises, setExercises] = useState(workout.exercises)
+
+  const handleAddExercise = (newExercise) => {
+    setExercises([...exercises, newExercise])
+  }
+  return (
+    <>
+      <div>
+        <ul>
+          Exercises:
+          {exercises.map((exercise) => {
+            return <Exercise key={exercise.id} exercise={exercise} />
+          })}
+        </ul>
+      </div>
+      <AddExerciseForm onAddExercise={handleAddExercise} />
+    </>
+  )
+}
--- src/components/Workout.jsx
+++ src/components/Workout.jsx
@@ -1,27 +1,11 @@
-import { useState } from 'react'
-import Exercise from './Exercise'
-import AddExerciseForm from './forms/AddExerciseForm'
+import ExerciseList from './ExerciseList'
 
 export default function Workout({ workout }) {
-  const [exercises, setExercises] = useState(workout.exercises)
-
-  const handleAddExercise = (newExercise) => {
-    setExercises([...exercises, newExercise])
-  }
-
   return (
     <div>
       <h3>Day: {workout.day}</h3>
       <p>Date: {workout.date}</p>
-      <div>
-        <ul>
-          Exercises:
-          {exercises.map((exercise) => {
-            return <Exercise key={exercise.id} exercise={exercise} />
-          })}
-        </ul>
-      </div>
-      <AddExerciseForm onAddExercise={handleAddExercise} />
+      <ExerciseList workout={workout} />
     </div>
   )
 }
