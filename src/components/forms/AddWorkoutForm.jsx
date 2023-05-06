3688ee397f6b493648e79f19da3775d5163531c8
Mar Kal <marc@example.com>
2023-05-06 21:46:25 +0200

    feat: add materialui date picker

--- src/components/forms/AddWorkoutForm.jsx
+++ src/components/forms/AddWorkoutForm.jsx
@@ -1,8 +1,14 @@
 import { useState } from 'react'
 import { PropTypes } from 'prop-types'
+import { LocalizationProvider } from '@mui/x-date-pickers'
+import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns'
+import { DatePicker } from '@mui/x-date-pickers/DatePicker'
+import { MobileDatePicker } from '@mui/x-date-pickers/MobileDatePicker'
+import { parseISO, addDays } from 'date-fns'
 
 export default function AddWorkoutForm({ onAddWorkout }) {
   const [date, setDate] = useState(new Date())
+  const maxDate = addDays(new Date(), 0)
 
   function handleAddWorkout(e) {
     e.preventDefault()
@@ -16,23 +22,21 @@ export default function AddWorkoutForm({ onAddWorkout }) {
     setDate(new Date())
   }
 
-  function handleDateChange(e) {
-    const selectedDate = new Date(e.target.value)
-    setDate(selectedDate)
+  function handleDateChange(newDate) {
+    setDate(newDate)
   }
 
   return (
     <form onSubmit={handleAddWorkout}>
       <p>Create a new workout</p>
-      <label>
-        Date:
-        <input
-          type="date"
-          value={date.toISOString().slice(0, 10)}
-          max={new Date().toISOString().slice(0, 10)}
+      <LocalizationProvider dateAdapter={AdapterDateFns}>
+        <MobileDatePicker
+          label="Date"
+          value={date}
+          maxDate={maxDate}
           onChange={handleDateChange}
         />
-      </label>
+      </LocalizationProvider>
       <button type="submit">Add Workout</button>
     </form>
   )
