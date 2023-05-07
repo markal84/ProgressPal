69b97e96c893b04af1f18e3b6ac303fc21a2e753
Mar Kal <marc@example.com>
2023-05-07 14:01:42 +0200

    feat: add datePicker to updateWorkout form

--- src/components/forms/UpdateWorkoutForm.jsx
+++ src/components/forms/UpdateWorkoutForm.jsx
@@ -1,51 +1,63 @@
-import { useState } from 'react'
+import { useState, useRef } from 'react'
 import { PropTypes } from 'prop-types'
+import Togglable from '../Togglable'
+import { LocalizationProvider } from '@mui/x-date-pickers'
+import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns'
+import { DatePicker } from '@mui/x-date-pickers/DatePicker'
+import { addDays, parseISO, formatISO } from 'date-fns'
+import enGB from 'date-fns/esm/locale/en-GB/index.js'
+import { Button, FormGroup, Typography } from '@mui/material'
 
 export default function UpdateWorkoutForm({ workout, onUpdateWorkout }) {
-  const [editMode, setEditMode] = useState(false)
   const [editedWorkout, setEditedWorkout] = useState(workout)
+  const maxDate = addDays(new Date(), 0)
+  const parsedDate = parseISO(editedWorkout.date)
+
+  const updateWorkoutFormRef = useRef()
 
   function handleUpdate(e) {
     e.preventDefault()
+    updateWorkoutFormRef.current.toggleVisibility()
 
     onUpdateWorkout(workout.id, editedWorkout)
-    setEditMode(false)
   }
 
-  function handleUpdateInputChange(e) {
-    const { name, value } = e.target
+  function handleUpdateDateChange(newDate) {
     setEditedWorkout((prevState) => ({
       ...prevState,
-      [name]: value
+      date: formatISO(newDate, { representation: 'date' })
     }))
   }
 
-  return (
-    <>
-      <button onClick={() => setEditMode(!editMode)}>
-        {editMode ? 'Cancel' : 'Edit'}
-      </button>
-      {editMode && (
-        <div>
-          <form>
-            <label htmlFor="date">
-              Date:
-              <input
-                type="date"
-                name="date"
-                value={editedWorkout.date}
-                max={new Date().toISOString().slice(0, 10)}
-                onChange={handleUpdateInputChange}
+  const updateWorkoutForm = () => {
+    return (
+      <Togglable buttonLabel="Edit workout" ref={updateWorkoutFormRef}>
+        <form onSubmit={handleUpdate}>
+          <Typography variant="h5" component="p" gutterBottom>
+            Edit Workout
+          </Typography>
+          <FormGroup>
+            <LocalizationProvider
+              dateAdapter={AdapterDateFns}
+              adapterLocale={enGB}
+            >
+              <DatePicker
+                label="Date"
+                value={parsedDate}
+                maxDate={maxDate}
+                onChange={handleUpdateDateChange}
               />
-            </label>
-            <button type="submit" onClick={handleUpdate}>
-              Update
-            </button>
-          </form>
-        </div>
-      )}
-    </>
-  )
+            </LocalizationProvider>
+          </FormGroup>
+          <Button variant="contained" type="submit" sx={{ mt: 2 }}>
+            Update
+          </Button>
+        </form>
+      </Togglable>
+    )
+  }
+
+  return <>{updateWorkoutForm()}</>
 }
 
 UpdateWorkoutForm.propTypes = {
