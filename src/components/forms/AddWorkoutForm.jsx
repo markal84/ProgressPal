adee71094d9be8474b786cc9a11ac93a14e04025
Mar Kal <marc@example.com>
2023-05-07 11:31:50 +0200

    feat: add materialui date picker with datefns

--- src/components/forms/AddWorkoutForm.jsx
+++ src/components/forms/AddWorkoutForm.jsx
@@ -1,10 +1,11 @@
 import { useState } from 'react'
 import { PropTypes } from 'prop-types'
+import { Box, Button, Typography } from '@mui/material'
 import { LocalizationProvider } from '@mui/x-date-pickers'
 import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns'
 import { DatePicker } from '@mui/x-date-pickers/DatePicker'
-import { MobileDatePicker } from '@mui/x-date-pickers/MobileDatePicker'
-import { parseISO, addDays } from 'date-fns'
+import { addDays } from 'date-fns'
+import enGB from 'date-fns/esm/locale/en-GB/index.js'
 
 export default function AddWorkoutForm({ onAddWorkout }) {
   const [date, setDate] = useState(new Date())
@@ -28,16 +29,22 @@ export default function AddWorkoutForm({ onAddWorkout }) {
 
   return (
     <form onSubmit={handleAddWorkout}>
-      <p>Create a new workout</p>
-      <LocalizationProvider dateAdapter={AdapterDateFns}>
-        <MobileDatePicker
-          label="Date"
-          value={date}
-          maxDate={maxDate}
-          onChange={handleDateChange}
-        />
-      </LocalizationProvider>
-      <button type="submit">Add Workout</button>
+      <Box display="flex" flexDirection="column" alignItems="center">
+        <Typography variant="h5" component="p" gutterBottom>
+          Create a new workout
+        </Typography>
+        <LocalizationProvider dateAdapter={AdapterDateFns} adapterLocale={enGB}>
+          <DatePicker
+            label="Date"
+            value={date}
+            maxDate={maxDate}
+            onChange={handleDateChange}
+          />
+        </LocalizationProvider>
+        <Button variant="contained" type="submit" sx={{ mt: 2 }}>
+          Add Workout
+        </Button>
+      </Box>
     </form>
   )
 }
