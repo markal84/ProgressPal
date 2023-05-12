6cdbe1da7e1fa17d5404325af170d8874e8f26ca
Mar Kal <marc@example.com>
2023-05-12 16:26:05 +0200

    refactor: home page state updates

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,17 +1,25 @@
+import { useState, useEffect } from 'react'
 import { PropTypes } from 'prop-types'
 import { totalWeight } from '../utilis/workoutInfo'
 
 export default function Home({ workouts }) {
-  const totalExercises = workouts.flatMap((workout) => workout.exercises).length
-  const exerciseArray = workouts.flatMap((workout) =>
-    workout.exercises
-      .filter((exercise) => !isNaN(Number(exercise.weight)))
-      .map((exercise) => [
-        Number(exercise.series),
-        Number(exercise.repetitions),
-        Number(exercise.weight)
-      ])
-  )
+  const [exerciseArray, setExerciseArray] = useState([])
+  const [totalExercises, setTotalExercises] = useState(0)
+
+  useEffect(() => {
+    const updatedExerciseArray = workouts.flatMap((workout) =>
+      workout.exercises
+        .filter((exercise) => !isNaN(Number(exercise.weight)))
+        .map((exercise) => [
+          Number(exercise.series),
+          Number(exercise.repetitions),
+          Number(exercise.weight)
+        ])
+    )
+
+    setExerciseArray(updatedExerciseArray)
+    setTotalExercises(workouts.flatMap((workout) => workout.exercises).length)
+  }, [workouts])
 
   totalWeight(exerciseArray)
 
